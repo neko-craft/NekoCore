d583c8bae2d376d7f0240becbecefb3a46f189d0
Shirasawa <764798966@qq.com>
2020-12-31 23:29:37 +0800

    Fix bugs

--- src/main/java/net/nekocraft/nekocore/Constants.java
+++ src/main/java/net/nekocraft/nekocore/Constants.java
@@ -19,7 +19,7 @@ public final class Constants {
 
     public static final String JOIN_MESSAGE_HEADER = "§b§m                       §r §a[§eNekoCraft§a] §b§m                      §r";
     public static final String JOIN_MESSAGE1 = "  §c由于服务器没有领地插件, 请不要随意拿取他人物品, 否则会直接封禁!";
-    public static final String JOIN_MESSAGE_FOOTER = "§b§m                                                          §r\n\n\n";
+    public static final String JOIN_MESSAGE_FOOTER = "§b§m                                                          §r\n\n";
     public static final TextComponent[] JOIN_MESSAGES = new TextComponent[9];
 
     public static final TextComponent[] RULES = new TextComponent[6];
